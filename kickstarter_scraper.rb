915462a4e764b6d9ba925d74cf02e6b12ee0be34
Brad Alexander <brad.alexander913@gmail.com>
2019-09-28 23:59:14 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,5 +10,7 @@ def create_project_hash
 end
 #projects: kickstarter.css("pli.project.grid_4")
 #title: project.css("h2.bbcard_name strong a").text
+#image link:  project.css("div.project-thumbnail a img").attribute("src").value
+#description: project.css("p.bbcard_blurb").text
 
 create_project_hash
